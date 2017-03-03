1a61da2aedea6a277dd2889105f2f13ba998537a
Dziugas <dziugas@graphity.org>
2017-03-03 19:34:47 +0200

    main class commited

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@
  * Created by User on 2017-03-03.
  */
 public class Main {
-    System.print.out()
+    public static void main (String args[])
+    {
+        System.out.print("Hello world!");
+    }
 }
