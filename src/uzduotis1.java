d66bf0846f676aea61ea79a30ca6995be4773e65
Dziugas <dziugas@graphity.org>
2017-03-03 20:10:06 +0200

    main class commited

---
+++ src/uzduotis1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by User on 2017-03-03.
+ */
+public class uzduotis1 {
+    public static void main(String[] args){
+        System.out.println("Ivesk savo varda");
+        Scanner sc= new Scanner(System.in);
+        String vardas= sc.nextLine();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("tavo vardas yra "+vardas);
+        }
+
+
+
+    }
+}
