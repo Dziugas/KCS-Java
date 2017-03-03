e0c2e7237473ffc005ef5290eb05855835962766
Dziugas <dziugas@graphity.org>
2017-03-03 21:17:58 +0200

    main class commited

--- src/uzduotis5.java
+++ src/uzduotis5.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
  * Created by User on 2017-03-03.
  */
 public class uzduotis5 {
+
+
     public static void main(String[] args) {
         System.out.println("Ivesk pirma skaiciu");
 
@@ -13,19 +15,32 @@ public class uzduotis5 {
         System.out.println("Ivesk antra skaiciu");
         int skaicius2 = sc.nextInt();
 
-        Veiksmas(skaicius1, skaicius2);
-        Veiksmas(skaicius1, skaicius2);
+        Suma((float) skaicius1, (float) skaicius2);
+        Suma2(skaicius1, skaicius2);
+        Skirtumas((float) skaicius1, (float) skaicius2);
+        Skirtumas2(skaicius1, skaicius2);
 
-        System.out.print("Siu skaiciu suma = " + Veiksmas(skaicius1, skaicius2) + ", o skirtumas = " + Veiksmas(skaicius1, skaicius2));
+        System.out.print("Siu skaiciu float suma = " + Suma((float)skaicius1,(float)skaicius2) + ", float skirtumas = " + Skirtumas((float)skaicius1, (float)skaicius2));
+        System.out.print("Siu skaiciu int suma = " + Suma2(skaicius1,skaicius2) + ", int skirtumas = " + Skirtumas2(skaicius1, skaicius2));
     }
 
-    public static float Veiksmas(float skaicius1, float skaicius2){
-        float suma = skaicius1+skaicius2;
+    public static float Suma(float skaicius1, float skaicius2) {
+        float suma = skaicius1 + skaicius2;
         return (suma);
     }
 
-    public static int Veiksmas(int skaicius1, int skaicius2) {
-        int skirtumas = skaicius1 - skaicius2;
+    public static float Skirtumas(float skaicius1, float skaicius2) {
+        float skirtumas = skaicius1 - skaicius2;
         return(skirtumas);
     }
+
+    public static int Skirtumas2(int skaicius1, int skaicius2) {
+        int skirtumas = skaicius1 - skaicius2;
+        return (skirtumas);
+    }
+    public static int Suma2(int skaicius1, int skaicius2) {
+        int suma = skaicius1 + skaicius2;
+        return(suma);
+    }
+
 }
