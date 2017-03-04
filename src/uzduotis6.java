c317db22ecdfd65a519d86258438761f23f0f4b8
Dziugas <dziugas@graphity.org>
2017-03-04 20:23:40 +0200

    main class commited

--- src/uzduotis6.java
+++ src/uzduotis6.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class uzduotis6 {
     public static void main (String[] args){
 
-        System.out.println("Labas, įvesk savo ūgį, metrais");
+        System.out.println("Ši programa apskaičiuos tavo kūno masės indeksą - KMI. Įvesk savo ūgį, metrais");
 
         Scanner sc = new Scanner(System.in);
         double ugis = sc.nextDouble();
@@ -14,7 +14,7 @@ public class uzduotis6 {
         System.out.println("Dabar įvesk savo svorį, kilogramais");
         double svoris = sc.nextDouble();
 
-        double KMI = svoris/(java.lang.Math.pow(ugis, 2));
+        double KMI = svoris/(Math.pow(ugis, 2));
         System.out.print("Tavo kūno masės indeksas yra "+KMI);
 //        KMI = masė (kg) / (ūgis(m))^2
     }
