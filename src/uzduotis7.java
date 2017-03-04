840db9a0be82dac15c51f38f3e869b6877ee9e3c
Dziugas <dziugas@graphity.org>
2017-03-04 20:40:25 +0200

    main class commited

---
+++ src/uzduotis7.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Created by User on 2017-03-04.
+ */
+public class uzduotis7 {
+    public static void main(String[] args) {
+        //       Realizuoti 1.6. užduotį panaudojant metodą, kurio parametrai būtų masė ir svoris, o
+        //       grąžinama reikšmė, kūno masės indeksas.
+        System.out.println("Ši programa apskaičiuos tavo kūno masės indeksą - KMI.");
+        System.out.println("Įvesk savo ūgį metrais, su kableliu");
+        Scanner sc = new Scanner(System.in);
+        double ugis = sc.nextDouble();
+        System.out.println("Dabar įvesk savo svorį, kilogramais");
+        double svoris = sc.nextDouble();
+
+        kmi(ugis, svoris);
+        System.out.print("Tavo kūno masės indeksas yra "+kmi(ugis, svoris));
+    }
+        public static double kmi(double ugis, double svoris){
+        double indeksas = svoris/(Math.pow(ugis, 2));
+        return(indeksas);
+    }
+}
+
