6cb7d192932419df77d473ba53eda66b58833e28
Dziugas <dziugas@graphity.org>
2017-03-06 21:22:10 +0200

    main class commited

---
+++ src/naujapamoka/uzduotis5/Uzduotis5.java
@@ -0,0 +1,11 @@
+package naujapamoka.uzduotis5;
+
+/**
+ * Created by User on 2017-03-06.
+ */
+public class Uzduotis5 {
+    public Uzduotis5(){
+        // paprašyti įvesti 5 skaičius
+        //baigus skaičių įvedimą turi būti atvaizduojama visų skaičių suma ir visi įvesti skaičiai
+    }
+}
