f6e3f7858a49071f50901d6d306f212cff963f41
Dziugas <dziugas@graphity.org>
2017-03-07 20:37:51 +0200

    main class commited

--- src/naujapamoka/uzduotis6/Uzduotis6.java
+++ src/naujapamoka/uzduotis6/Uzduotis6.java
@@ -5,17 +5,26 @@ import java.util.Scanner;
 /**
  * Created by User on 2017-03-07.
  */
+//ctrl+alt+l -> gražiai sulygiuoja kodą
 public class Uzduotis6 {
-    public Uzduotis6(){
-        String[] masyvas = new String[5];
-        System.out.println("Sveiki, reikės įvesti 5 žodžius");
-        Scanner sc=new Scanner(System.in);
-        for (int i = 0; i <masyvas.length ; i++) {
-            System.out.println("Įveskite "+(i+1)+" žodį");
-             masyvas[i] = sc.nextLine();
+    String[] masyvas;
+
+    public Uzduotis6() {
+        System.out.println("Sveiki, reikės įvesti keletą žodžių");
+    }
+    public void nuskaitymas() {
+        System.out.println("Pirma įveskite masyvo ilgį");
+        Scanner sc = new Scanner(System.in);
+        int ilgis = sc.nextInt();
+        masyvas = new String[ilgis];
+        for (int i = 0; i < masyvas.length; i++) {
+            System.out.println("Įveskite " + (i + 1) + " žodį");
+            masyvas[i]=sc.next();//next vietoj nextLine - kitaip kažkodėl prašoka pirmą žodį
         }
-        for (int i = 0; i <masyvas.length ; i++) {
-            System.out.println((i+1)+" žodis yra "+masyvas[i]);
+    }
+    public void spausdinimas() {
+        for (int i = 0; i < masyvas.length; i++) {
+            System.out.println((i + 1) + " žodis yra " + masyvas[i]);
         }
     }
 }
